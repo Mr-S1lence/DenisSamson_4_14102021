dafd2c5af85cc9a09d84855b38043e873ab59616
Mr-S1lence <samsond.co@free.fr>
2021-10-26 16:08:52 +0200

    suppression du code inutile dans fichier CSS

---
+++ purgecss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        'index.html',
+        'contact.html'
+    ],
+    css: [
+        './css/bootstrap.min.css',
+        './css/font-awesome.min.css',
+        './css/et-line.min.css',
+        './style.min.css'
+    ],
+    keyframes: true,
+    output: './css/purified/',
+    rejected: true,
+    variables: true
+  }
