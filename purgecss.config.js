c13ca52e0faa98ee2aa85df4d96069d2c64158db
Mr-S1lence <samsond.co@free.fr>
2021-11-05 02:56:06 +0100

    correction erreur js/css purified

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,16 +1,17 @@
 module.exports = {
     content: [
         'index.html',
-        'contact.html'
+        'contact.html',
+        'js/*.js'
     ],
     css: [
         './css/bootstrap.min.css',
         './css/font-awesome.min.css',
         './css/et-line.min.css',
-        './style.min.css'
+        './css/style.min.css'
     ],
     keyframes: true,
-    output: './css/purified/',
+    output: './css/purified2/',
     rejected: true,
     variables: true
   }
